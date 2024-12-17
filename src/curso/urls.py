e01ce3de1ef0ea7e8ced2332753f839f2a145b03
Alejo Kubar <alejokubar@gmail.com>
2024-12-17 16:33:40 -0300

    Estructura del proyecto

---
+++ src/curso/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+
+from .views import about, index
+
+app_name = "curso"
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("about/", about, name="about"),
+]
+
+
