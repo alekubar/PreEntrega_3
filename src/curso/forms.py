e746ac9e17a10aa8b5e3bd215caf199154a1436d
Alejo Kubar <alejokubar@gmail.com>
2024-12-17 20:44:49 -0300

    cambios finales

--- src/curso/forms.py
+++ src/curso/forms.py
@@ -5,3 +5,18 @@ class CursoForm(forms.ModelForm):
     class Meta:
         model=Curso
         fields = "__all__"
+
+class ComisionForm(forms.ModelForm):
+    class Meta:
+        model=Comision
+        fields = "__all__"
+        widgets = {
+            'fecha_inicio': forms.DateInput(attrs={'type': 'date'}), 
+            }
+
+
+class AlumnoForm(forms.ModelForm):
+    class Meta:
+        model=Alumno
+        fields = "__all__"
+ 
--- src/curso/urls.py
+++ src/curso/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 
-from .views import about, index, curso_list, comision_list, alumno_list
+from .views import about, index, curso_list, comision_list, alumno_list, comision_create, curso_create, alumno_create
 
 app_name = "curso"
 
@@ -11,6 +11,9 @@ urlpatterns = [
     path("curso/list/", curso_list, name="curso_list"),
     path("comision/list/", comision_list, name="comision_list"),
     path("alumno/list/", alumno_list, name="alumno_list"),
+    path("curso/create/", curso_create, name="curso_create"),
+    path("comision/create/", comision_create, name="comision_create"),
+    path("alumno/create/", alumno_create, name="alumno_create"),
 ]
 
 
