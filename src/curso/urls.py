2f2a2969ce2e735af2d65900d009a0a2c2aacaf2
Alejo Kubar <alejokubar@gmail.com>
2024-12-17 19:02:12 -0300

    cambios

--- src/curso/urls.py
+++ src/curso/urls.py
@@ -1,13 +1,16 @@
 
 from django.urls import path
 
-from .views import about, index
+from .views import about, index, curso_list, comision_list, alumno_list
 
 app_name = "curso"
 
 urlpatterns = [
     path("", index, name="index"),
     path("about/", about, name="about"),
+    path("curso/list/", curso_list, name="curso_list"),
+    path("comision/list/", comision_list, name="comision_list"),
+    path("alumno/list/", alumno_list, name="alumno_list"),
 ]
 
 
--- src/curso/views.py
+++ src/curso/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from .models import Curso, Comision, Alumno
+from .forms import CursoForm
 # Create your views here.
 def index(request):
     return render(request, "curso/index.html")
 
 def about(request):
     return render(request,"curso/about.html" )
+
+def curso_list(request):
+    query = Curso.objects.all()
+    context = {"object_list": query}
+    return render(request, "curso/curso_list.html", context)
+
+def curso_create(request):
+    if request.method == "GET":
+        form = CursoForm()
+        if request.method == "POST":
+            form = CursoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("curso:")
+
+def comision_list(request):
+    query = Comision.objects.all()
+    context = {"object_list": query}
+    return render(request, "curso/comision_list.html", context)
+
+def alumno_list(request):
+    query = Alumno.objects.all()
+    context = {"object_list": query}
+    return render(request, "curso/alumno_list.html", context)
